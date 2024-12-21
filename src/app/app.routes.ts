af229137036d1ce3b0fd1740cc31d11468c35e10
Debuga Dev <debugadev@gmail.com>
2024-12-21 05:26:14 -0300

    Inclusão das rotas para os tutoriais

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,9 @@ import { ContatoComponent } from './components/contato/contato.component';
 import { PoliticaPrivacidadeComponent } from './components/politica-privacidade/politica-privacidade.component';
 import { TermosUsoComponent } from './components/termos-uso/termos-uso.component';
 import { BlogsComponent } from './components/blogs/blogs.component';
+import { AlgumasCoisasSobreJava1Component } from './tutorials/algumas-coisas-sobre-java-1/algumas-coisas-sobre-java-1.component';
+import { AlgumasCoisasSobreJava2Component } from './tutorials/algumas-coisas-sobre-java-2/algumas-coisas-sobre-java-2.component';
+import { CriandoLogEmaaplicacoesSpringBootComponent } from './tutorials/criando-log-emaaplicacoes-spring-boot/criando-log-emaaplicacoes-spring-boot.component';
 
 export const routes: Routes = [
   { path: "", component: HomeComponent },
@@ -15,5 +18,8 @@ export const routes: Routes = [
   { path: "quem-somos", component: QuemSomosComponent },
   { path: "contato", component: ContatoComponent },
   { path: "politica-privacidade", component: PoliticaPrivacidadeComponent },
-  { path: "termos-uso", component: TermosUsoComponent }
+  { path: "termos-uso", component: TermosUsoComponent },
+  { path: "algumas-coisas-sobre-java-1", component: AlgumasCoisasSobreJava1Component },
+  { path: "algumas-coisas-sobre-java-2", component: AlgumasCoisasSobreJava2Component },
+  { path: "criando-log-emaaplicacoes-spring-boot", component: CriandoLogEmaaplicacoesSpringBootComponent }
 ];
