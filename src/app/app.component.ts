bf90b336337e4c9138498a7831753af80a9db2ed
Debuga Dev <debugadev@gmail.com>
2024-12-20 22:46:07 -0300

    Configurações iniciais para o ambiente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    NavbarComponent,
+    BannerComponent,
+    RouterOutlet,
+    FooterComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'debugadev-site';
+  title = 'debugadev-site-gpt';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { VideosComponent } from './components/videos/videos.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { QuemSomosComponent } from './components/quem-somos/quem-somos.component';
+import { ContatoComponent } from './components/contato/contato.component';
+import { PoliticaPrivacidadeComponent } from './components/politica-privacidade/politica-privacidade.component';
+import { TermosUsoComponent } from './components/termos-uso/termos-uso.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "home", component: HomeComponent },
+  { path: "videos", component: VideosComponent },
+  { path: "blog", component: BlogComponent },
+  { path: "quem-somos", component: QuemSomosComponent },
+  { path: "contato", component: ContatoComponent },
+  { path: "politica-privacidade", component: PoliticaPrivacidadeComponent },
+  { path: "termos-uso", component: TermosUsoComponent }
+];
