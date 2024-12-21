ecf442b22681b7222cc3205b5beded6eeadb8b0d
Debuga Dev <debugadev@gmail.com>
2024-12-21 03:00:14 -0300

    Correção da rota para acessar a pagina de blogs.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { VideosComponent } from './components/videos/videos.component';
-import { BlogComponent } from './components/blog/blog.component';
 import { QuemSomosComponent } from './components/quem-somos/quem-somos.component';
 import { ContatoComponent } from './components/contato/contato.component';
 import { PoliticaPrivacidadeComponent } from './components/politica-privacidade/politica-privacidade.component';
 import { TermosUsoComponent } from './components/termos-uso/termos-uso.component';
+import { BlogsComponent } from './components/blogs/blogs.component';
 
 export const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "home", component: HomeComponent },
   { path: "videos", component: VideosComponent },
-  { path: "blog", component: BlogComponent },
+  { path: "blogs", component: BlogsComponent },
   { path: "quem-somos", component: QuemSomosComponent },
   { path: "contato", component: ContatoComponent },
   { path: "politica-privacidade", component: PoliticaPrivacidadeComponent },
